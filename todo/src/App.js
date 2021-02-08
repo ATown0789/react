a9d5d1209e1e145b17fae15cd2c83b9aed92d349
Anthony Collini <collini.anthony@gmail.com>
2021-02-08 06:39:52 -0600

    Add transitions to todo app using react-spring

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,24 +2,28 @@
 import {useState} from 'react';
 import './App.css';
 import Todo from './Components/Todo';
-import TodoForm from './Components/TodoForm'
+import TodoForm from './Components/TodoForm';
+import {useSpring, useTransition, animated} from 'react-spring';
 
 
 function App() {
   const [todoList, setTodoList] = useState(
     [{
       text: "my todo1",
-      completed: false
+      completed: false,
+      id: 0
     },
     {
       text: "my todo 2",
-      completed: false
+      completed: false,
+      id: 1
     }  
   ]); 
 
    
   const addTodo = text => {
-    const newTodoList = [...todoList, {text}];
+    const newId = todoList[todoList.length-1].id+1;
+    const newTodoList = [...todoList, {text, id:newId}];
     setTodoList(newTodoList);
   }
 
@@ -35,13 +39,39 @@ function App() {
     setTodoList(newTodoList);
   }
 
+  const deleteAll = () => {
+    setTodoList([]);
+  }
+
+  const todoAnimation = useTransition(todoList, item =>( item.id), {
+    from: {opacity:0},
+    enter: {opacity: 1},
+    leave: {opacity: 0},
+    config: {duration: 1000}
+  });
+  
+  const delAnimation = useSpring(
+    {opacity: 1, 
+      from: {opacity: 0},
+      config: {duration: 1000}
+    });
+
   return (
     <div className="App">
           <h1>My Todo List</h1>
           <TodoForm addTodo = {addTodo} />
-          {todoList.map((todo, index) => (
+          {/* {todoList.map((todo, index) => (
             <Todo removeTodo = {removeTodo} completeTodo = {completeTodo} index = {index} key = {index} todo = {todo}  />
-          ))}
+          ))} */}
+          {todoAnimation.map(({item, key, props},index) => 
+            <animated.div key = {key} style = {props}>
+              <Todo removeTodo = {removeTodo} 
+                completeTodo = {completeTodo} 
+                index = {index} 
+                key = {index} 
+                todo = {item}  />
+            </animated.div>)}
+          <animated.div style = {delAnimation}>{todoList.length > 1 ? <button id = "del-all" className = "delete" onClick = {deleteAll}>Delete All</button> : null }</animated.div>
     </div>
   );
 }
--- todo/src/Components/Todo.jsx
+++ todo/src/Components/Todo.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React from 'react';
 import '../App.css';
 
 
-function Todo({todo, index, completeTodo, removeTodo}) {
+
+
+
+function Todo({stuff, todo, index, completeTodo, removeTodo}) {
+    console.log(stuff);
     return (
+        
         <div style = {{textDecoration: todo.completed ? 'line-through' : ''}} className = "todo">
             <input id = "completed" name = "completed" type = "checkbox" onClick = {() => completeTodo(index)}/>
             <span id ="todo-text">{todo.text}</span>
-            <button onClick = {() => removeTodo(index)}><i class="fas fa-trash-alt"></i></button>
+            <button className = "delete" onClick = {() => removeTodo(index)}><i className="fas fa-trash-alt"></i></button>
         </div>
-    )
+
+//This is a working animation for just the entrance of the todo
+// const todoAnimation = useSpring({
+//     from: {opacity: 0},
+//     to: {opacity: 1},
+//     config: {duration: 600}
+// });
+
+// return (
+//     <animated.div style = {todoAnimation}>
+//     <div style = {{textDecoration: todo.completed ? 'line-through' : ''}} className = "todo">
+//         <input id = "completed" name = "completed" type = "checkbox" onClick = {() => completeTodo(index)}/>
+//         <span id ="todo-text">{todo.text}</span>
+//         <button className = "delete" onClick = {() => removeTodo(index)}><i class="fas fa-trash-alt"></i></button>
+//     </div>
+//     </animated.div>
+// )
+)
 }
 
 export default Todo;
